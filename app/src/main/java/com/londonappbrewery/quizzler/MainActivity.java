a932dad2ebe52dd1861a9e06ab746e450da0d51e
londonappbrewery <londonappbrewery@gmail.com>
2016-01-15 16:47:47 +0000

    Initial commit

---
+++ app/src/main/java/com/londonappbrewery/quizzler/MainActivity.java
@@ -0,0 +1,39 @@
+package com.londonappbrewery.quizzler;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    // TODO: Declare constants here
+
+
+    // TODO: Declare member variables here:
+
+
+    // TODO: Uncomment to create question bank
+//    private TrueFalse[] mQuestionBank = new TrueFalse[] {
+//            new TrueFalse(R.string.question_1, true),
+//            new TrueFalse(R.string.question_2, true),
+//            new TrueFalse(R.string.question_3, true),
+//            new TrueFalse(R.string.question_4, true),
+//            new TrueFalse(R.string.question_5, true),
+//            new TrueFalse(R.string.question_6, false),
+//            new TrueFalse(R.string.question_7, true),
+//            new TrueFalse(R.string.question_8, false),
+//            new TrueFalse(R.string.question_9, true),
+//            new TrueFalse(R.string.question_10, true),
+//            new TrueFalse(R.string.question_11, false),
+//            new TrueFalse(R.string.question_12, false),
+//            new TrueFalse(R.string.question_13,true)
+//    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}

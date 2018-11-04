e7dcbad85420f1aa03b85658d65ed9a43fe2bfb4
afr741@mun.ca <afr741@mun.ca>
2018-11-04 11:54:21 -0330

    set Onclick listener and implemented Toast message

--- app/src/main/java/com/londonappbrewery/quizzler/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/quizzler/MainActivity.java
@@ -2,7 +2,10 @@ package com.londonappbrewery.quizzler;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -34,9 +37,25 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    mTrueButton = (Button) findViewById(R.id.true_button);
-    mFalseButton = (Button) findViewById(R.id.false_button);
 
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mFalseButton = (Button) findViewById(R.id.false_button);
 
+        View.OnClickListener myListener = new View.OnClickListener() {
+            @Override
+            public void OnClick(View v) {
+                Log.d("Quizzler","Button pressed!" );
+                Toast.makeText(getApplicationContext(), "True Pressed!", Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        mTrueButton.setOnClickListener(myListener);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast myToast = Toast.makeText(getApplicationContext(), "False pressed", Toast.LENGTH_SHORT);
+                myToast.show();
+            }
+        });
     }
 }

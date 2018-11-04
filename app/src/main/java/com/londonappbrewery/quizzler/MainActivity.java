e9c8a3c0b97182a82809475e558cd1f7986aec66
afr741@mun.ca <afr741@mun.ca>
2018-11-04 17:33:01 -0330

    added truefalse.java, retrive questions from aarray and display

--- app/src/main/java/com/londonappbrewery/quizzler/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/quizzler/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
@@ -15,23 +16,25 @@ public class MainActivity extends Activity {
     // TODO: Declare member variables here:
     Button mTrueButton;
     Button mFalseButton;
-
-    // TODO: Uncomment to create question bank
-//    private TrueFalse[] mQuestionBank = new TrueFalse[] {
-//            new TrueFalse(R.string.question_1, true),
-//            new TrueFalse(R.string.question_2, true),
-//            new TrueFalse(R.string.question_3, true),
-//            new TrueFalse(R.string.question_4, true),
-//            new TrueFalse(R.string.question_5, true),
-//            new TrueFalse(R.string.question_6, false),
-//            new TrueFalse(R.string.question_7, true),
-//            new TrueFalse(R.string.question_8, false),
-//            new TrueFalse(R.string.question_9, true),
-//            new TrueFalse(R.string.question_10, true),
-//            new TrueFalse(R.string.question_11, false),
-//            new TrueFalse(R.string.question_12, false),
-//            new TrueFalse(R.string.question_13,true)
-//    };
+    TextView mQuetionTextView;
+    int mIndex;
+    int mQuestion;
+  //   TODO: Uncomment to create question bank
+    private TrueFalse[] mQuestionBank = new TrueFalse[] {
+            new TrueFalse(R.string.question_1, true),
+            new TrueFalse(R.string.question_2, true),
+            new TrueFalse(R.string.question_3, true),
+            new TrueFalse(R.string.question_4, true),
+            new TrueFalse(R.string.question_5, true),
+            new TrueFalse(R.string.question_6, false),
+            new TrueFalse(R.string.question_7, true),
+            new TrueFalse(R.string.question_8, false),
+            new TrueFalse(R.string.question_9, true),
+            new TrueFalse(R.string.question_10, true),
+            new TrueFalse(R.string.question_11, false),
+            new TrueFalse(R.string.question_12, false),
+            new TrueFalse(R.string.question_13,true)
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,9 +43,16 @@ public class MainActivity extends Activity {
 
         mTrueButton = findViewById(R.id.true_button);
         mFalseButton = findViewById(R.id.false_button);
+        mQuetionTextView = (TextView) findViewById(R.id.question_text_view);
+
+        mQuestion = mQuestionBank[mIndex].getQuestionID();
+
+        mQuetionTextView.setText(mQuestion);
 
         // create onclick listener object
-        View.OnClickListener myListner = new View.OnClickListener() {
+
+        //set onclick listener to true button
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
@@ -51,10 +61,7 @@ public class MainActivity extends Activity {
                 //Show toast message without creating new object
                 Toast.makeText(getApplicationContext(),"True Button Clicked!",Toast.LENGTH_SHORT).show();
             }
-        };
-
-        //set onclick listener to true button
-        mTrueButton.setOnClickListener(myListner);
+        });
 
         //creating anonymous listener for false button
         mFalseButton.setOnClickListener(new View.OnClickListener() {
---
+++ app/src/main/java/com/londonappbrewery/quizzler/TrueFalse.java
@@ -0,0 +1,31 @@
+package com.londonappbrewery.quizzler;
+
+public class TrueFalse {
+
+    private int mQuestionID;
+    private boolean mAnswer;
+
+
+    public int getQuestionID() {
+        return mQuestionID;
+    }
+
+    public void setQuestionID(int questionID) {
+        mQuestionID = questionID;
+    }
+
+    public boolean isAnswer() {
+        return mAnswer;
+    }
+
+    public void setAnswer(boolean answer) {
+        mAnswer = answer;
+    }
+
+    public TrueFalse(int questionResourceID, boolean trueOrFalse)
+    {
+        mQuestionID = questionResourceID;
+        mAnswer = trueOrFalse;
+
+    }
+}

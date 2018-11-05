8e82169f60c3f0e5abe467acd0fdb72e4533336b
afr741@mun.ca <afr741@mun.ca>
2018-11-05 12:33:19 -0330

    check user's answer, progress bar, score and etc

--- app/src/main/java/com/londonappbrewery/quizzler/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/quizzler/MainActivity.java
@@ -1,84 +1,161 @@
-package com.londonappbrewery.quizzler;
+package com.londonappbrewery.quizzler_complete;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.londonappbrewery.quizzler.R;
+
+import static android.widget.Toast.makeText;
+
 public class MainActivity extends Activity {
 
-    // TODO: Declare constants here
-
-
-    // TODO: Declare member variables here:
+    // Member variables accessible in all the methods of the MainActivity:
     Button mTrueButton;
     Button mFalseButton;
-    TextView mQuetionTextView;
+    TextView mScoreTextView;
+    TextView mQuestionTextView;
+    ProgressBar mProgressBar;
     int mIndex;
+    int mScore;
     int mQuestion;
-  //   TODO: Uncomment to create question bank
-    private TrueFalse[] mQuestionBank = new TrueFalse[] {
-            new TrueFalse(R.string.question_1, true),
-            new TrueFalse(R.string.question_2, true),
-            new TrueFalse(R.string.question_3, true),
-            new TrueFalse(R.string.question_4, true),
-            new TrueFalse(R.string.question_5, true),
-            new TrueFalse(R.string.question_6, false),
-            new TrueFalse(R.string.question_7, true),
-            new TrueFalse(R.string.question_8, false),
-            new TrueFalse(R.string.question_9, true),
-            new TrueFalse(R.string.question_10, true),
-            new TrueFalse(R.string.question_11, false),
-            new TrueFalse(R.string.question_12, false),
-            new TrueFalse(R.string.question_13,true)
+    Toast mToastMessage; // For keeping track if a Toast message is being shown.
+
+    // Create question bank using the TrueFalse class for each item in the array
+    @NonNull
+    private com.londonappbrewery.quizzler_complete.TrueFalse[] mQuestionBank = new com.londonappbrewery.quizzler_complete.TrueFalse[] {
+            new com.londonappbrewery.quizzler_complete.TrueFalse(R.string.question_1, true),
+            new com.londonappbrewery.quizzler_complete.TrueFalse(R.string.question_2, true),
+            new com.londonappbrewery.quizzler_complete.TrueFalse(R.string.question_3, true),
+            new com.londonappbrewery.quizzler_complete.TrueFalse(R.string.question_4, true),
+            new com.londonappbrewery.quizzler_complete.TrueFalse(R.string.question_5, true),
+            new com.londonappbrewery.quizzler_complete.TrueFalse(R.string.question_6, false),
+            new com.londonappbrewery.quizzler_complete.TrueFalse(R.string.question_7, true),
+            new com.londonappbrewery.quizzler_complete.TrueFalse(R.string.question_8, false),
+            new com.londonappbrewery.quizzler_complete.TrueFalse(R.string.question_9, true),
+            new com.londonappbrewery.quizzler_complete.TrueFalse(R.string.question_10, true),
+            new com.londonappbrewery.quizzler_complete.TrueFalse(R.string.question_11, false),
+            new com.londonappbrewery.quizzler_complete.TrueFalse(R.string.question_12, false),
+            new com.londonappbrewery.quizzler_complete.TrueFalse(R.string.question_13,true)
     };
 
+    // Declaring constants here. Rather than a fixed number, choosing to make it a function
+    // of the length of the question bank (the number of questions)
+    final int PROGRESS_BAR_INCREMENT = (int) Math.ceil(100.0 / mQuestionBank.length);
+
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // In API 26 'casting' is no longer needed.
+        // For API 25 and lower: mTrueButton = (Button) findViewById(R.id.true_button);
+        // For API 26 and higher can use: mTrueButton = findViewById(R.id.true_button);
         mTrueButton = findViewById(R.id.true_button);
         mFalseButton = findViewById(R.id.false_button);
-        mQuetionTextView = (TextView) findViewById(R.id.question_text_view);
+        mQuestionTextView = findViewById(R.id.question_text_view);
+        mScoreTextView = findViewById(R.id.score);
+        mProgressBar = findViewById(R.id.progress_bar);
+
+        // Restores the 'state' of the app upon screen rotation:
+        if (savedInstanceState != null) {
+            mScore = savedInstanceState.getInt("ScoreKey");
+            mIndex = savedInstanceState.getInt("IndexKey");
+            mScoreTextView.setText("Score " + mScore + "/" + mQuestionBank.length);
+        } else {
+            mScore = 0;
+            mIndex = 0;
+        }
 
         mQuestion = mQuestionBank[mIndex].getQuestionID();
+        mQuestionTextView.setText(mQuestion);
 
-        mQuetionTextView.setText(mQuestion);
-
-        // create onclick listener object
-
-        //set onclick listener to true button
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-
-                Log.d("Quizzler", " True Button Clicked!!!");
-
-                //Show toast message without creating new object
-                Toast.makeText(getApplicationContext(),"True Button Clicked!",Toast.LENGTH_SHORT).show();
+            public void onClick(View v) {
+                checkAnswer(true);
+                updateQuestion();
             }
         });
 
-        //creating anonymous listener for false button
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-
-                Log.d("Quizzler", " False Button Clicked!!!");
-
-                //create Toast message
-                Toast toastMessage  = Toast.makeText(getApplicationContext(),"False Button Clicked!",Toast.LENGTH_SHORT);
-                toastMessage.show();
+            public void onClick(View v) {
+                checkAnswer(false);
+                updateQuestion();
             }
         });
 
+    }
 
-        //TrueFalse exampleQuestion = new TrueFalse(R.string.question_1,true);
+    private void updateQuestion() {
+        // This takes the modulus. Not a division.
+        mIndex = (mIndex + 1) % mQuestionBank.length;
 
+        // Present an alert dialog if we reach the end.
+        if(mIndex == 0) {
+            AlertDialog.Builder alert = new AlertDialog.Builder(this);
+            alert.setTitle("Game Over");
+            alert.setCancelable(false);
+            alert.setMessage("You scored " + mScore + " points!");
+            alert.setPositiveButton("Close Application", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    finish();
+                }
+            });
+            alert.show();
+        }
+
+        mQuestion = mQuestionBank[mIndex].getQuestionID();
+        mQuestionTextView.setText(mQuestion);
+        mProgressBar.incrementProgressBy(PROGRESS_BAR_INCREMENT);
+        mScoreTextView.setText("Score " + mScore + "/" + mQuestionBank.length);
+    }
+
+    private void checkAnswer(boolean userSelection) {
+
+        boolean correctAnswer = mQuestionBank[mIndex].isAnswer();
+
+        // Can cancel the Toast message if there is one on screen and a new answer
+        // has been submitted.
+        if (mToastMessage != null) {
+            mToastMessage.cancel();
+        }
+
+        if(userSelection == correctAnswer) {
+            mToastMessage = makeText(getApplicationContext(), R.string.correct_toast, Toast.LENGTH_SHORT);
+            mScore = mScore + 1;
+
+        } else {
+            mToastMessage = Toast.makeText(getApplicationContext(), R.string.incorrect_toast, Toast.LENGTH_LONG);
+        }
+
+        mToastMessage.show();
 
     }
+
+    // This callback is received when the screen is rotated so we can save the 'state'
+    // of the app in a 'bundle'.
+    @Override
+    public void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+
+        outState.putInt("ScoreKey", mScore);
+        outState.putInt("IndexKey", mIndex);
+    }
+
 }
+
+
+
--- app/src/main/java/com/londonappbrewery/quizzler/TrueFalse.java
+++ app/src/main/java/com/londonappbrewery/quizzler/TrueFalse.java
@@ -1,31 +1,36 @@
-package com.londonappbrewery.quizzler;
+package com.londonappbrewery.quizzler_complete;
 
+// This is the model class that represents a single quiz question.
 public class TrueFalse {
 
+    // These are the placeholders for the question resource id and the correct answer
     private int mQuestionID;
     private boolean mAnswer;
 
+    // This is the constructor that will be called when a new quiz question is created.
+    public TrueFalse(int questionResourceID, boolean trueOrFalse) {
+        mQuestionID = questionResourceID;
+        mAnswer = trueOrFalse;
+    }
 
+    // This method gives us access to info stored in the (private) question id.
     public int getQuestionID() {
         return mQuestionID;
     }
 
-    public void setQuestionID(int questionID) {
-        mQuestionID = questionID;
-    }
-
+    // This method gives us access to info stored in the (private) mAnswer.
     public boolean isAnswer() {
         return mAnswer;
     }
 
-    public void setAnswer(boolean answer) {
-        mAnswer = answer;
-    }
+    // Not actually using the setters at the moment. Users are not creating questions.
 
-    public TrueFalse(int questionResourceID, boolean trueOrFalse)
-    {
-        mQuestionID = questionResourceID;
-        mAnswer = trueOrFalse;
+//    public void setQuestionID(int questionID) {
+//        mQuestionID = questionID;
+//    }
 
-    }
+//    public void setAnswer(boolean answer) {
+//        mAnswer = answer;
+//    }
+
 }
